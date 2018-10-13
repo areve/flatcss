63834c3791426ba07386c383c841c69ffead89f4
areve <areve@example.com>
2018-10-13 16:58:37 +0100

    table margin added and heading line heights

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = (env, argv) => {
     new WebpackAutoInject({
       components: {
         AutoIncreaseVersion: autoIncreaseVersion,
-        InjectAsComment: true,
+        InjectAsComment: false,
         InjectByTag: true
       }
     })
